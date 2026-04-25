d62328969ecd8420fd7ee263283691211719abd8
mdawso <dawsonmathew08@gmail.com>
2026-04-26 00:09:32 +0100

    cmake cleanup and basic raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include <raylib.h>
+
 int main() {
-    
+
+    SetTargetFPS(60);
+    InitWindow(800, 600, "GBA");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
 }
